55dee390fdafb3b302785530b04cc2f4026bf786
EmersonTeles <emersonteles21@gmail.com>
2021-07-26 19:03:03 -0300

    montagem da estrutura basica

--- src/App.js
+++ src/App.js
@@ -1,11 +1,73 @@
-import './App.css';
+import './App.css'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 
 function App() {
+  const [heroes, setHeroes] = useState({})
+  const heroesApi = `https://www.superheroapi.com/api/${process.env.REACT_APP_TOKEN_HEROESAPI}`
+
+  const getHero = async (id) => {
+    await axios.get(`${heroesApi}/${id}`)
+    .then((response)=> {
+      setHeroes(response.data)
+    })
+    .catch((error)=>{
+      console.log(error)
+    })
+  }
+
+   
+  useEffect(()=>{
+    getHero(346)
+    // eslint-disable-next-line
+  }, [])
+
   return (
-    <div className="App">
-      
-    </div>
-  );
+      <div className="App">
+          <header> 
+            <h1>SUPER HERÓIS</h1>
+          </header>
+
+          <div className="content">
+          {heroes && (
+            <ul className="heroes-list">
+              <li key={heroes.id}>
+                {heroes.image && (
+                  <img src={heroes.image.url} alt={`${heroes.name}_img`}/>
+                )}
+                <h3>{heroes.name}</h3>
+              </li>
+              <li>
+                {heroes.image && (
+                  <img src={heroes.image.url} alt={`${heroes.name}_img`}/>
+                )}
+                <h3>{heroes.name}</h3>
+              </li>
+              <li>
+                {heroes.image && (
+                  <img src={heroes.image.url} alt={`${heroes.name}_img`}/>
+                )}
+                <h3>{heroes.name}</h3>
+              </li>
+              <li>
+                {heroes.image && (
+                  <img src={heroes.image.url} alt={`${heroes.name}_img`}/>
+                )}
+                <h3>{heroes.name}</h3>
+              </li>
+              <li>
+                {heroes.image && (
+                  <img src={heroes.image.url} alt={`${heroes.name}_img`}/>
+                )}
+                <h3>{heroes.name}</h3>
+              </li>
+            </ul>
+            )
+          }
+          </div>
+      </div>
+    )
 }
 
-export default App;
+export default App
