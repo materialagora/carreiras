c039f2bcfdadb4b92369b115af28269cc8b0b04e
EmersonTeles <emersonteles21@gmail.com>
2021-07-27 13:53:09 -0300

    ajustando botao de login

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,11 +1,12 @@
 import './styles.css'
+import user_icon from '../../Assets/user-icon.svg'
 export default function Header (){
     return(
         <nav className="navbar"> 
             <div className="header_upper">
               <h1>geeklouco.com</h1>
               <input type="text" placeholder="Herói? Vilão? O que você imaginar aqui..."/>
-              <a href="#" className="login">Login</a>
+              <a href="#" className="login">Login<img src={user_icon} alt="user"/></a>
             </div>
             <ul>
               <li><a href="">Universo Marvel</a></li>
