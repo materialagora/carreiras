566ffe9e94d31cfa28e75c8b8076511e0a26ff76
Emerson Teles <54421630+EmersonTeles@users.noreply.github.com>
2021-08-18 19:07:06 -0300

    Consertando link da api && Resolvendo Cors

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: `https://www.superheroapi.com/api/${process.env.REACT_APP_TOKEN_HEROESAPI}/`
+    baseURL: `https://www.superheroapi.com/api.php/${process.env.REACT_APP_TOKEN_HEROESAPI}/`
 })
     
 export default api
