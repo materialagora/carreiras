7c97319be62ff657872fd8e94e452e62e5e77aa1
Samantha Cardoso <samanthacardoso.dev@outlook.com>
2021-10-03 15:31:28 -0300

    feat: add styled-components and global styles

---
+++ src/globalStyles.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components'
+import background from './assets/background.jpg'
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    outline: 0;
+    border: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #root { 
+    height: 100%;
+  }
+
+  body {
+    font-family: Roboto, sans-serif;
+    background: url(${background}) fixed;
+    background-size: cover;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`
