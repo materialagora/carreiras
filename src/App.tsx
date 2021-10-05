e060d7ceda67d2f77bcea99d07b058b977d9f125
Samantha <samanthacardoso.dev@outlook.com>
2021-10-04 22:26:03 -0300

    ref: remove hook

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@ import React from 'react'
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "./routes";
 
-import { FavoritesProvider } from './hooks/useFavorites'
-
 import { GlobalStyles } from "./globalStyles";
 
 function App() {
   return (
-    <FavoritesProvider>
+    <>
       <BrowserRouter>
         <Routes />
       </BrowserRouter>
       <GlobalStyles />
-    </FavoritesProvider>
+    </>
   );
 }
 
