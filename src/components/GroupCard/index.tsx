a9cc8cab93635716259c44fd988df24bebe5297d
Victor Oliveira <vggaia10@gmail.com>
2022-10-31 19:11:51 -0300

    style: adjust the GroupCard layout

---
+++ src/components/GroupCard/index.tsx
@@ -0,0 +1,77 @@
+import { Button, Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React, { useEffect, useState } from 'react'
+import { api } from '../../services/api'
+import DeleteIcon from '@mui/icons-material/Delete'
+import IconButton from '@mui/material/IconButton'
+
+interface GroupCardProps {
+  id: string
+  removeFromGroup: any
+  groupName: string
+}
+
+interface Hero {
+  name: string
+  image: string
+}
+
+const GroupCard: React.FC<GroupCardProps> = ({
+  id,
+  removeFromGroup,
+  groupName
+}) => {
+  const [hero, setHero] = useState<Hero | null>(null)
+
+  const getHeroWithId = async (id: string) => {
+    const data = await (await api.get(id)).data
+    setHero({ name: data.name, image: data.image.url })
+  }
+
+  useEffect(() => {
+    getHeroWithId(id)
+  }, [])
+
+  return (
+    <>
+      <Box>
+        {!hero ? (
+          <Typography>Caregando...</Typography>
+        ) : (
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              p: 2,
+              borderRadius: 5,
+              background: '#F3F3F3'
+            }}
+          >
+            <Typography sx={{ display: 'flex', alignItems: 'center' }}>
+              {hero.name}
+            </Typography>
+            <Box sx={{ display: 'flex', justifyContent: 'center', gap: 5 }}>
+              <IconButton
+                onClick={() => removeFromGroup(groupName, id)}
+                aria-label="delete"
+                size={'small'}
+              >
+                <DeleteIcon fontSize="small" />
+              </IconButton>
+              <img
+                style={{
+                  width: 60,
+                  height: 70,
+                  borderRadius: 8
+                }}
+                src={hero.image}
+              />
+            </Box>
+          </Box>
+        )}
+      </Box>
+    </>
+  )
+}
+
+export default GroupCard
