a915a5c2b38691dfa4b61a9c86cc1787e6900bf8
Miguel Buca <miguelpedrobuca@gmail.com>
2022-11-08 18:40:36 +0100

    chore[header]: created with searchPersonInput

--- src/pages/root/index.tsx
+++ src/pages/root/index.tsx
@@ -1,15 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { useRootState } from "./state";
-
 export const Root = () => {
-  const {
-    query: { data },
-  } = useRootState();
-
-  console.log("data: ", data);
-
   return (
     <div>
       <Link to={"hero/3"}>hero 30</Link>
