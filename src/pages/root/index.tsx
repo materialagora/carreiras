c485ff5237c32d4b77d6701ce58b3d0581a63873
Miguel Buca <miguelpedrobuca@gmail.com>
2022-11-09 06:37:13 +0100

    feat[root]: show by alignment type

--- src/pages/root/index.tsx
+++ src/pages/root/index.tsx
@@ -5,17 +5,31 @@ import { CardPerson } from "components/organisms";
 import { useRootState } from "./state";
 
 export const Root = () => {
-  const { people } = useRootState();
+  const { tabShow, people, villain, hero, filter, handlerFilter, getStyles } =
+    useRootState();
 
   return (
-    <div>
-      <div className="border-solid border-b border-[rgba(0,0,0,.06)] dark:border-[rgba(255,255,255,.06)]">
-        ola mundo
+    <div className="py-[30px]">
+      <div className="mb-[30px] overflow-hidden border-solid border-b border-[rgba(0,0,0,.06)] dark:border-[rgba(255,255,255,.06)]">
+        {tabShow.map((item, index) => {
+          return (
+            <span
+              className={getStyles(item)}
+              key={index}
+              onClick={() => handlerFilter(item)}
+            >
+              {item}
+            </span>
+          );
+        })}
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 max-xl:grid-cols-8 gap-[20px] py-[30px]">
-        {people.map((item, index) => (
-          <CardPerson {...item} key={index} />
-        ))}
+      <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 max-xl:grid-cols-8 gap-[20px]">
+        {filter === "All" &&
+          people.map((item, index) => <CardPerson {...item} key={index} />)}
+        {filter === "Hero" &&
+          hero.map((item, index) => <CardPerson {...item} key={index} />)}
+        {filter === "Villain" &&
+          villain.map((item, index) => <CardPerson {...item} key={index} />)}
       </div>
     </div>
   );
--- src/pages/root/state.ts
+++ src/pages/root/state.ts
@@ -1,10 +1,15 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useSelector } from "react-redux";
 
 import { RootState } from "redux/store";
 
+import { TabShowType } from "./types";
+
 export const useRootState = () => {
   const people = useSelector((state: RootState) => state.people);
+  const [filter, setFilter] = useState<TabShowType>("All");
+
+  const tabShow: TabShowType[] = ["All", "Hero", "Villain"];
 
   const hero = useMemo(() => {
     const list = people.data.filter(
@@ -20,9 +25,24 @@ export const useRootState = () => {
     return list;
   }, [people]);
 
+  const handlerFilter = (value: TabShowType) => setFilter(value);
+
+  const getStyles = (value: TabShowType) => {
+    const styled =
+      filter === value
+        ? "text-primary-1 bg-[rgba(0,0,0,.06)] dark:bg-[rgba(255,255,255,.06)]"
+        : "";
+
+    return "font-bold cursor-pointer uppercase p-[.5rem_1rem] " + styled;
+  };
+
   return {
+    tabShow,
     people: people.data,
     hero,
     villain,
+    filter,
+    handlerFilter,
+    getStyles,
   };
 };
