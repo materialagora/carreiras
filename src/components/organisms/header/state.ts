7a486d32b784def055ec2fcbab9f4cfbcc1b8168
Miguel Buca <miguelpedrobuca@gmail.com>
2022-11-18 00:23:59 -0800

    fix[header]: replaced to goBack function

--- src/components/organisms/header/state.ts
+++ src/components/organisms/header/state.ts
@@ -4,11 +4,13 @@ import { useStorage } from "hooks";
 import { IPerson } from "interfaces/person";
 import { refresh } from "redux/slices";
 import { RootState } from "redux/store";
+import { browser } from "utils/browser";
 
 export const useHeaderState = () => {
   const dispatch = useDispatch();
   const storage = useStorage();
   const { match } = useSelector((state: RootState) => state.route);
+  const { goBack } = browser();
 
   const handlerSearchPersonInput = (data?: IPerson[] | undefined) => {
     data && dispatch(refresh(data));
@@ -23,7 +25,7 @@ export const useHeaderState = () => {
     const deletedPersons: string[] = storage.deletedPersons;
     deletedPersons.push(id);
     storage.add("DELETED_PERSONS", JSON.stringify(deletedPersons));
-    window.history.back();
+    goBack();
   };
 
   return {
